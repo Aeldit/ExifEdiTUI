545a469b3a88ca20a9ed31879e91adc571485ef0
aeldit <aeldit.66@proton.me>
2025-01-28 01:02:42 +0200

    EXIF chunks can now be read

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,15 @@
-use core::fmt;
 use std::{env::args, fs};
 
 mod png;
-use png::{Ihdr, IHDR_CHUNK_SIZE};
+use png::{ExifChunk, EXIF_CHUNK_SIZE};
 mod conversions;
 use conversions::*;
 
-/// Exif data of an image
-///
-/// tag: Each tag is assigned a unique 2-byte number to identify the field. The tag numbers in the Exif 0th
-///      IFD and 1st IFD are all the same as the TIFF tag numbers.
-///
-/// data_type:  1 = BYTE An 8-bit unsigned integer.
-///             2 = ASCII An 8-bit byte containing one 7-bit ASCII code.
-///                 The final byte is terminated with NULL.
-///             3 = SHORT A 16-bit (2-byte) unsigned integer,
-///             4 = LONG A 32-bit (4-byte) unsigned integer,
-///             5 = RATIONAL Two LONGs. The first LONG is the numerator and the second LONG
-///                 expresses the denominator.
-///             7 = UNDEFINED An 8-bit byte that may take any value depending on the field definition.
-///             9 = SLONG A 32-bit (4-byte) signed integer (2's complement notation).
-///             10 = SRATIONAL Two SLONGs. The first SLONG is the numerator and the second SLONG is
-///                  the denominator.
-///
-/// count: The number of values. It should be noted carefully that the count is not the sum of the bytes. In the
-///        case of one value of SHORT (16 bits), for example, the count is '1' even though it is 2 Bytes
-///
-/// value_offset: This tag records the offset from the start of the TIFF header to the position where the value itself is
-///               recorded. In cases where the value fits in 4 Bytes, the value itself is recorded. If the value is smaller
-///               than 4 Bytes, the value is stored in the 4-Byte area starting from the left, i.e., from the lower end of
-///               the byte offset area. For example, in big endian format, if the type is SHORT and the value is 1, it is
-///               recorded as 00010000.H.
-///               Note that field Interoperability shall be recorded in sequence starting from the smallest tag number.
-///               There is no stipulation regarding the order or position of tag value (Value) recording.
-///
-struct ExifData {
-    tag: u16,
-    data_type: u16,
-    count: u64,
-    value_offset: u64,
-}
-
-impl ExifData {
-    pub fn from(tag: &[u8], data_type: &[u8], count: &[u8], value_offset: &[u8]) -> Self {
-        Self {
-            tag: match u8_array_to_u16(tag.to_vec()) {
-                Some(converted_tag) => converted_tag,
-                None => panic!("invalid data tag"),
-            },
-            data_type: match u8_array_to_u16(data_type.to_vec()) {
-                Some(converted_data_type) => converted_data_type,
-                None => panic!("invalid data type"),
-            },
-            count: match u8_array_to_u64(count.to_vec()) {
-                Some(converted_count) => converted_count,
-                None => panic!("invalid count"),
-            },
-            value_offset: match u8_array_to_u64(value_offset.to_vec()) {
-                Some(converted_value_offset) => converted_value_offset,
-                None => panic!("invalid value_offset"),
-            },
-        }
-    }
-}
-
-impl fmt::Display for ExifData {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "(tag: {}, type: {}, count: {}, value_offset: {})",
-            self.tag, self.data_type, self.count, self.value_offset
-        )
-    }
-}
-
-#[derive(Debug)]
+/*#[derive(Debug)]
 enum ImagesTypes {
     Png,
     Jpeg,
-}
+}*/
 
 fn main() {
     let args: Vec<String> = args().collect();
@@ -96,33 +27,27 @@ fn main() {
         Err(e) => return eprintln!("{e:?}"),
     };
 
-    let exif_chars = vec![101, 88, 73, 102]; //eXIf
-    let exif_start_idx = index_of_sub_array(img_contents.clone(), exif_chars).unwrap() + 4;
-    /*println!("{}", *img_contents.get(exif_start_idx + 1).unwrap() as char);
-
-    println!("{}", *img_contents.get(256).unwrap());
-
-    println!(
-        "{}",
-        u8_array_to_u16(img_contents[255..257].to_vec()).unwrap()
+    let exif_magic = vec![0x65, 0x58, 0x49, 0x66]; // eXIf
+    let exif_chunk_start = match index_of_sub_array(img_contents.clone(), exif_magic.clone()) {
+        Some(magic_start) => magic_start + exif_magic.len(),
+        None => return,
+    };
+    let exif_chunk = ExifChunk::from(
+        img_contents[exif_chunk_start..exif_chunk_start + EXIF_CHUNK_SIZE].as_ref(),
     );
+    println!("{}", exif_chunk);
 
-    println!(
-        "{}",
-        u8_array_to_u32(img_contents[255..259].to_vec()).unwrap()
-    );*/
-
-    let block = img_contents[exif_start_idx + 1..exif_start_idx + 23].to_vec();
-    let exif_data = ExifData::from(
-        block[0..2].as_ref(),
-        block[2..4].as_ref(),
-        block[4..12].as_ref(),
-        block[12..20].as_ref(),
-    );
-    println!("{}", exif_data);
+    let mut next_idx = exif_chunk_start + EXIF_CHUNK_SIZE + 4;
+    for _ in 0..4 {
+        println!(
+            "{}",
+            ExifChunk::from(img_contents[next_idx..next_idx + EXIF_CHUNK_SIZE].as_ref())
+        );
+        next_idx += EXIF_CHUNK_SIZE + 4;
+    }
 
     // PNG
-    let png_magic = vec![0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A];
+    /*let png_magic = vec![0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A];
 
     let image_type = if img_contents.starts_with(&png_magic) {
         ImagesTypes::Png
@@ -141,5 +66,5 @@ fn main() {
     let ihdr_chunk = Ihdr::from(
         img_contents[ihdr_chunk_start_idx..ihdr_chunk_start_idx + IHDR_CHUNK_SIZE].as_ref(),
     );
-    println!("{}", ihdr_chunk);
+    println!("{}", ihdr_chunk);*/
 }
--- src/png.rs
+++ src/png.rs
@@ -1,14 +1,81 @@
 use core::fmt;
 
-use crate::{u8_array_to_u32, u8_array_to_u64};
+use crate::{u8_array_to_u16, u8_array_to_u32, u8_array_to_u64};
 
+// In bytes
+pub const EXIF_CHUNK_SIZE: usize = 20;
 pub const IHDR_CHUNK_SIZE: usize = 13;
 
-pub struct PngChunk {
-    length: u64,
-    chunk_type: Vec<u8>,
-    chunk_data: Vec<u8>,
-    crc: u64,
+/// Exif data of an image
+///
+/// tag: Each tag is assigned a unique 2-byte number to identify the field. The tag numbers in the Exif 0th
+///      IFD and 1st IFD are all the same as the TIFF tag numbers.
+///
+/// data_type:  1 = BYTE An 8-bit unsigned integer.
+///             2 = ASCII An 8-bit byte containing one 7-bit ASCII code.
+///                 The final byte is terminated with NULL.
+///             3 = SHORT A 16-bit (2-byte) unsigned integer,
+///             4 = LONG A 32-bit (4-byte) unsigned integer,
+///             5 = RATIONAL Two LONGs. The first LONG is the numerator and the second LONG
+///                 expresses the denominator.
+///             7 = UNDEFINED An 8-bit byte that may take any value depending on the field definition.
+///             9 = SLONG A 32-bit (4-byte) signed integer (2's complement notation).
+///             10 = SRATIONAL Two SLONGs. The first SLONG is the numerator and the second SLONG is
+///                  the denominator.
+///
+/// count: The number of values. It should be noted carefully that the count is not the sum of the bytes. In the
+///        case of one value of SHORT (16 bits), for example, the count is '1' even though it is 2 Bytes
+///
+/// value_offset: This tag records the offset from the start of the TIFF header to the position where the value itself is
+///               recorded. In cases where the value fits in 4 Bytes, the value itself is recorded. If the value is smaller
+///               than 4 Bytes, the value is stored in the 4-Byte area starting from the left, i.e., from the lower end of
+///               the byte offset area. For example, in big endian format, if the type is SHORT and the value is 1, it is
+///               recorded as 00010000.H.
+///               Note that field Interoperability shall be recorded in sequence starting from the smallest tag number.
+///               There is no stipulation regarding the order or position of tag value (Value) recording.
+///
+pub struct ExifChunk {
+    tag: u16,
+    data_type: u16,
+    count: u64,
+    value_offset: u64,
+}
+
+impl ExifChunk {
+    pub fn from(slice: &[u8]) -> Self {
+        Self {
+            tag: match u8_array_to_u16(slice[0..2].to_vec()) {
+                Some(converted_tag) => converted_tag,
+                None => panic!("Invalid data tag"),
+            },
+            data_type: match u8_array_to_u16(slice[2..4].to_vec()) {
+                Some(converted_data_type) => converted_data_type,
+                None => panic!("Invalid data type"),
+            },
+            count: match u8_array_to_u64(slice[4..12].to_vec()) {
+                Some(converted_count) => converted_count,
+                None => panic!("Invalid count"),
+            },
+            value_offset: match u8_array_to_u64(slice[12..20].to_vec()) {
+                Some(converted_value_offset) => converted_value_offset,
+                None => panic!("Invalid value_offset"),
+            },
+        }
+    }
+
+    pub fn is_little_endian(&self) -> bool {
+        false
+    }
+}
+
+impl fmt::Display for ExifChunk {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "EXIF {{\n\ttag: {},\n\ttype: {},\n\tcount: {},\n\tvalue_offset: {}\n}}",
+            self.tag, self.data_type, self.count, self.value_offset
+        )
+    }
 }
 
 pub struct Ihdr {
@@ -19,26 +86,6 @@ pub struct Ihdr {
     compression_method: u8,
     filter_method: u8,
     interlace_method: u8,
-}
-
-impl PngChunk {
-    pub fn from(length: &[u8], chunk_type: &[u8], chunk_data: &[u8], crc: &[u8]) -> Self {
-        Self {
-            length: match u8_array_to_u64(length.to_vec()) {
-                Some(len) => len,
-                None => panic!("PNG: Error in the length"),
-            },
-            chunk_type: match chunk_type.len() {
-                4 => chunk_type.to_vec(),
-                _ => panic!("PNG: Error in the chunk_type"),
-            },
-            chunk_data: Vec::from(chunk_data),
-            crc: match u8_array_to_u64(crc.to_vec()) {
-                Some(c) => c,
-                None => panic!("PNG: Error in the crc"),
-            },
-        }
-    }
 }
 
 impl Ihdr {
